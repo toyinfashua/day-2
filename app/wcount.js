4243e52067f16987da04b5fcafe39119099fb2d2
toyinfashua <oluwatoyinfashua@gmail.com>
2016-11-29 16:08:01 +0100

    wordcount test and source code

--- app/wcount.js
+++ app/wcount.js
@@ -1,19 +1,23 @@
-function words(string, cutOff) {
-  var cleanString = string.replace(/[\.,-\/#!$%\^&\*;:{}=\-_`~()]/g,""),
-      words = cleanString.split(' '),
-      frequencies = {},
-      word, frequency, i;
-
-  for( i=0; i<words.length; i++ ) {
-    if ( word[i] == frequencies ) {
-      count++;
+var words = {
+  words: function wordCount(str) 
+  {
+  var count = {}; // this would create an object to store the result of the computation
+  var wCheck = str.replace(/[\n\t]/gi, ' ')
+             .replace(/ {2,}/g, " ").split(' '); // usedto replace special characters with a single space
+  wCheck.forEach (function (word) 
+  {
+  if (count.hasOwnProperty(word)) // method to check the property of the object
+    {
+      count[word]+=1; //if it does have the property 'word', the count increases by 1
+    } 
+  else 
+    {
+      count[word]=1 // if it does not the count remains 1 or is equal to 1
     }
-    frequencies[word] = frequencies[word] || 0;
-    frequencies[word]++;
-  }
-  
-  words ( "asdf asdf asdf", 'a');
+  });
 
-  return words.sort(function (a,b) { return frequencies[b] -frequencies[a];}).slice(0,cutOff).toString();
-}
-module.exports = cleanString;
+  return count; 
+    }   //returns new object
+  }
+
+module.exports = words;
--- tests/wctest.js
+++ tests/wctest.js
@@ -1,4 +1,3 @@
-(function() {
 'use strict'
 
 var chai = require('chai');
@@ -6,60 +5,60 @@ var expect = chai.expect;
 
 var wcount = require('../app/wcount.js');
 
+
 describe("words()", function() {
   it("counts one word", function() {
     var expectedCounts = { word: 1 };
-    expect(wcount.words("word")).to.equal(expectedCounts);
+    expect(wcount.words("word")).to.deep.equal(expectedCounts);
   });
 
   it("counts one of each", function() {
     var expectedCounts = { one: 1, of: 1, each: 1 };
-    expect(wcount.words("one of each")).to.equal(expectedCounts);
+    expect(wcount.words("one of each")).to.deep.equal(expectedCounts);
   });
 
   it("counts multiple occurrences", function() {
     var expectedCounts = { one: 1, fish: 4, two: 1, red: 1, blue: 1 };
-    expect(wcount.words("one fish two fish red fish blue fish")).to.equal(expectedCounts);
+    expect(wcount.words("one fish two fish red fish blue fish")).to.deep.equal(expectedCounts);
   });
 
   it("includes punctuation", function() {
     var expectedCounts = { car: 1, ":": 2, carpet: 1, as: 1, java: 1, "javascript!!&@$%^&": 1 };
-    expect(wcount.words("car : carpet as java : javascript!!&@$%^&")).to.equal(expectedCounts);
+    expect(wcount.words("car : carpet as java : javascript!!&@$%^&")).to.deep.equal(expectedCounts);
   });
 
   it("includes numbers", function() {
     var expectedCounts = { testing: 2, 1: 1, 2: 1 };
-    expect(wcount.words("testing 1 2 testing")).to.equal(expectedCounts);
+    expect(wcount.words("testing 1 2 testing")).to.deep.equal(expectedCounts);
   });
 
   it("respects case", function() {
     var expectedCounts = { go: 1, Go:1, GO:1 };
-    expect(wcount.words("go Go GO")).to.equal(expectedCounts);
+    expect(wcount.words("go Go GO")).to.deep.equal(expectedCounts);
   });
 
   it("counts properly international characters", function() {
     var expectedCounts = { "¡Hola!": 1, "¿Qué": 1, "tal?": 1, "Привет!": 1 };
-    expect(wcount.words("¡Hola! ¿Qué tal? Привет!")).to.equal(expectedCounts);
+    expect(wcount.words("¡Hola! ¿Qué tal? Привет!")).to.deep.equal(expectedCounts);
   });
 
   it("counts multiline", function() {
     var expectedCounts = { hello: 1, world: 1 };
-    expect(wcount.words("hello\nworld")).to.equal(expectedCounts);
+    expect(wcount.words("hello\nworld")).to.deep.equal(expectedCounts);
   });
 
   it("counts tabs", function() {
     var expectedCounts = { hello: 1, world: 1 };
-    expect(wcount.words("hello\tworld")).to.equal(expectedCounts);
+    expect(wcount.words("hello\tworld")).to.deep.equal(expectedCounts);
   });
 
   it("counts multiple spaces as one", function() {
     var expectedCounts = { hello: 1, world: 1 };
-    expect(wcount.words("hello  world")).to.equal(expectedCounts);
+    expect(wcount.words("hello  world")).to.deep.equal(expectedCounts);
   });
 
   it("handles properties that exist on Object’s prototype", function() {
     var expectedCounts = { reserved: 1, words : 1, like :1,  prototype: 1, and : 1, toString: 1,  "ok?": 1};
-    expect(wcount.words("reserved words like prototype and toString ok?")).to.equal(expectedCounts);
+    expect(wcount.words("reserved words like prototype and toString ok?")).to.deep.equal(expectedCounts);
   });
-});
 });
