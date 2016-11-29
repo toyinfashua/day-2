3d41436b3bdde205dae4e2cc7729543085a00811
toyinfashua <oluwatoyinfashua@gmail.com>
2016-11-29 17:08:33 +0100

    minmax commit

--- app/minmax.js
+++ app/minmax.js
@@ -1,21 +1,21 @@
 var quest = {
 
-findMinMax: function(arr) {
-var max = 0;
-var min = arr[0];
-  for(var i = 0; i < arr.length; i++) {
-    if (arr[i] > max) {
-      max = arr[i];
+  findMinMax: function(arr) {
+  var max = 0;
+  var min = arr[0];
+    for(var i = 0; i < arr.length; i++) {
+      if (arr[i] > max) {
+        max = arr[i];
+      }
+      if (arr[i] < min) {
+        min = arr[i];
+      }
     }
-    if (arr[i] < min) {
-      min = arr[i];
+    if (min === max) {
+      return[min];
+    } else {
+      return[min, max];
     }
   }
-  if (min === max) {
-    return[min];
-  } else {
-    return[min, max];
-  }
-}
 }
 module.exports = quest;
