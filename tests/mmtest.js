4947484012dea86724322beaf35544d0ba693c87
toyinfashua <oluwatoyinfashua@gmail.com>
2016-11-29 16:49:19 +0100

    second cmmit mimnax

--- tests/mmtest.js
+++ tests/mmtest.js
@@ -3,22 +3,22 @@
   var chai = require('chai');
   var expect = chai.expect;
 
-  var wcount = require('../app/wcount.js');
+  var quest = require('../app/minmax.js');
 
   describe('Min-Max Numbers in a List: ', function () {
 
     describe('Return the min and max number in the list in a new list follows `[min, max]`', function () {
 
       it('should return [1,4] for [1, 2, 3 , 4]', function () {
-        expect(findMinMax([1, 2, 3, 4])).toEqual([1, 4]);
+        expect(quest.findMinMax([1, 2, 3, 4])).to.equal([1, 4]);
       });
 
       it('should return [4, 6] for [6, 4]', function () {
-        expect(findMinMax([6, 4])).toEqual([4, 6]);
+        expect(quest.findMinMax([6, 4])).to.equal([4, 6]);
       });
 
       it('should return [2, 78] for [4, 66, 6, 44, 7, 78, 8, 68, 2]', function () {
-        expect(findMinMax([4, 66, 6, 44, 7, 78, 8, 68, 2])).toEqual([2, 78]);
+        expect(quest.findMinMax([4, 66, 6, 44, 7, 78, 8, 68, 2])).to.equal([2, 78]);
       });
 
     });
@@ -26,7 +26,7 @@
     describe('Return the number in the list in a new list follows `[min]` when the number is the min and max number in that list', function () {
 
       it('should return [4] for [4, 4, 4, 4]', function () {
-        expect(findMinMax([4, 4, 4, 4])).toEqual([4]);
+        expect(quest.findMinMax([4, 4, 4, 4])).to.equal([4]);
       });
 
     });
